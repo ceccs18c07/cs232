d85305366dbe747ef66e692bb3f912a98ecdd0b3
ceccs18c07 <60247301+ceccs18c07@users.noreply.github.com>
2020-04-20 13:26:47 +0530

    Add files via upload

---
+++ 2004/add.pl
@@ -0,0 +1,13 @@
+#! /usr/bin/perl
+
+# read elements 
+print "Enter the first number \n";
+$choise1 = <> ;
+print "Enter the second number \n";
+$choise2 = <> ;
+# compute sum
+$sum = $choise1 + $choise2;
+# print sum
+print "\n  The result is $sum" ;
+
+
---
+++ 2004/odd.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+#Read the value
+print "Enter the number:\n";
+$Number = <>;
+
+if ($Number%2==0)
+{
+   print "$Number is even \n";
+}
+else 
+{
+   print "$Number is odd \n";
+} 
